b15aa0d87b7a51848237f9be4874285e7960aa46
Osauyi <victorimafidon17@gmail.com>
2023-06-15 09:41:31 +0100

    changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 
+let URL = "https://en.wikipedia.org/wiki/Cross-origin_resource_sharing"
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,6 +25,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", function(req, res){
+   var date = new Date()
+   res.json({utc : date})
+})
 
 
 // listen for requests :)
